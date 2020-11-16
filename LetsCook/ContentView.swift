62d312d5e496ceedd0b274c8c0ee108eaf3bd8a1
zpandza <zxp6097@g.rit.edu>
2020-11-16 18:22:05 +0100

    Initial Commit

---
+++ LetsCook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LetsCook
+//
+//  Created by Zvonimir Pandza on 16.11.2020..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LetsCook/LetsCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsCookApp.swift
+//  LetsCook
+//
+//  Created by Zvonimir Pandza on 16.11.2020..
+//
+
+import SwiftUI
+
+@main
+struct LetsCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
